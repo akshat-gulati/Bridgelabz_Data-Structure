c5aa57800278c32adf0a96763c19162028441578
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2024-12-20 10:17:14 +0530

    Akshat update LinkedListPractice.js to add 'Merge 2 sorted LinkedList'

--- LinkedList/LinkedListPractice.js
+++ LinkedList/LinkedListPractice.js
@@ -133,3 +133,41 @@ class Solution {
         return true;
     }
 }
+
+// Merge 2 sorted LinkedList ---------------------------------
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    let dummy = new ListNode(0);
+    let temp = dummy;
+
+    while (list1 !== null && list2 !== null) {
+        if (list1.val <= list2.val) {
+            temp.next = list1;
+            list1 = list1.next;
+        } else {
+            temp.next = list2;
+            list2 = list2.next;
+        }
+        temp = temp.next;
+    }
+
+    if (list1 !== null) {
+        temp.next = list1;
+    } else {
+        temp.next = list2;
+    }
+
+    return dummy.next;
+};
